e45c861dc8900a4bf5751ac983d9389c79cb3cf6
shack254 <123meshack@gmail.com>
2019-06-10 11:11:04 +0300

    hover effect

--- js/scripts.js
+++ js/scripts.js
@@ -24,6 +24,12 @@ $(document).ready(function(){
         $("#product-hide").fadeOut();
         $("#product").fadeIn();
     });
+        $('.over-lay').hover(function () {
+        $(".project-titie").animate({opacity:'1'});
+        },
+        function () {
+        $(".text-apperance").animate({opacity:'0.5'});
+    })
     $("#submittion").submit(function(event){
         var name = $("input#name").val();
         var email =$("input#email").val();
